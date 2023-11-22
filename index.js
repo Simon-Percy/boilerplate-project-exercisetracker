9cd654cae15f218863820fa2afe954e16e169d67
SimonPercy <mmmRaliphada809@gmail.com>
2023-11-22 22:11:30 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+//database connecting
+const dbURI = process.env.DB;
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log("connected"))
+  .catch((err) => console.log(err));
+
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/api/users", (req, res) => {
+  const username = req.body.username;
+  res.json({ username });
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
---
+++ users.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: "fcc_test",
+  description: "test",
+  duration: 60,
+  date: "Mon Jan 01 1990",
+  _id: "5fb5853f734231456ccb3b05",
+});
